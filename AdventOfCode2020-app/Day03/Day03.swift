f0ce1e8dd53649e71d6affd1013c5513a289c08b
Andy Lee <aglee@mac.com>
2020-12-02 06:15:01 -0500

    Added Day 3 files, in preparation for tomorrow.

---
+++ AdventOfCode2020-app/Day03/Day03.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class Day03: DayNN {
+	override var expectedPart1TestResults: [Int : String] {
+		return [:]
+	}
+	override var expectedPart2TestResults: [Int : String] {
+		return [:]
+	}
+
+	init() {
+		super.init("TBD")
+	}
+
+	// MARK: - Solving
+
+	override func solvePart1(inputLines: [String]) -> String {
+		return super.solvePart1(inputLines: inputLines)
+	}
+
+	override func solvePart2(inputLines: [String]) -> String {
+		return super.solvePart2(inputLines: inputLines)
+	}
+
+	// MARK: - Private stuff
+
+}
+
+
---
+++ AdventOfCode2020-app/Day03/Day03_Discussion.md
@@ -0,0 +1,6 @@
+# Day 3 - TBD
+
+<https://adventofcode.com/2020/day/3>
+
+
+
