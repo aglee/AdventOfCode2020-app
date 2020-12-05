8ad06e848855099d596b1b510650e3f87809a33a
Andy Lee <aglee@mac.com>
2020-12-05 00:03:35 -0500

    Added Day 5 files.

---
+++ AdventOfCode2020-app/Day05/Day05.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+class DayXX: DayNN {
+//	override var expectedPart1TestResults: [Int : String] {
+//		return [:]
+//	}
+//	override var expectedPart2TestResults: [Int : String] {
+//		return [:]
+//	}
+
+	init() {
+		super.init("DayXX")
+	}
+
+	// MARK: - Solving
+
+//	override func solvePart1(inputLines: [String]) -> String {
+//	}
+
+//	override func solvePart2(inputLines: [String]) -> String {
+//	}
+
+	// MARK: - Private stuff
+
+}
+
+
