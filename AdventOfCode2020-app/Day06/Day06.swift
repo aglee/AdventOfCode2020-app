5e70df0fd02bd5ac64da98b2f8b07146d5d57478
Andy Lee <aglee@mac.com>
2020-12-06 05:44:46 -0500

    Fiddled more with the tests for Day 6.

--- AdventOfCode2020-app/Day06/Day06.swift
+++ AdventOfCode2020-app/Day06/Day06.swift
@@ -6,47 +6,61 @@ class Day06: DayNN {
 		self.part1Tests = [
 			testPart1(fileNumber: 1, expectedResult: "6"),
 			test(fileNumber: 2,
-				 function: {
-					let groups = groupedLines($0)
-					let setsOfLetters = groups.map { self.questionsAnsweredYesBy_ANY_(inGroup: $0) }
-					let counts = setsOfLetters.map { $0.count }
-					return counts.map { String($0) }.joined(separator: " + ")
-				 },
+				 function: { self.sumString(self.countsForPart1($0)) },
 				 expectedResult: "3 + 3 + 3 + 1 + 1"),
 		]
 		self.part2Tests = [
-			testPart2(fileNumber: 2, expectedResult: "6"),
+			test(fileNumber: 2,
+				 function: { self.sumString(self.countsForPart2($0)) },
+				 expectedResult: "3 + 0 + 1 + 1 + 1"),
 		]
+	}
+
+	/// `[1, 2, 3] => "1 + 2 + 3"`
+	private func sumString(_ nums: [Int]) -> String {
+		return nums.map { String($0) }.joined(separator: " + ")
 	}
 
 	// MARK: - Solving
 
-	func questionsAnsweredYesBy_ANY_(inGroup group: [String]) -> Set<Character> {
-		var questionsAnswered = Set<Character>()
-		for line in group {
-			questionsAnswered.formUnion(line)
+	private func countsForPart1(_ inputLines: [String]) -> [Int] {
+		func questionsAnsweredYesBy_ANY_(inGroup group: [String]) -> Set<Character> {
+			var questionsAnswered = Set<Character>()
+			for line in group {
+				questionsAnswered.formUnion(line)
+			}
+			//print(questionsAnswered.sorted())
+			return questionsAnswered
 		}
-		//print(questionsAnswered.sorted())
-		return questionsAnswered
+
+		let groups = groupedLines(inputLines)
+		let setsOfLetters = groups.map { questionsAnsweredYesBy_ANY_(inGroup: $0) }
+		return setsOfLetters.map { $0.count }
 	}
 
 	override func solvePart1(inputLines: [String]) -> String {
-		let groups = groupedLines(inputLines)
-		return String(groups.reduce(0, { $0 + questionsAnsweredYesBy_ANY_(inGroup: $1).count }))
+		let total = countsForPart1(inputLines).reduce(0, { $0 + $1 })
+		return String(total)
 	}
 
-	func questionsAnsweredYesBy_ALL_(inGroup group: [String]) -> Set<Character> {
-		var questionsAnswered = Set("abcdefghijklmnopqrstuvwxyz")
-		for line in group {
-			questionsAnswered.formIntersection(line)
+	private func countsForPart2(_ inputLines: [String]) -> [Int] {
+		func questionsAnsweredYesBy_ALL_(inGroup group: [String]) -> Set<Character> {
+			var questionsAnswered = Set("abcdefghijklmnopqrstuvwxyz")
+			for line in group {
+				questionsAnswered.formIntersection(line)
+			}
+			//print(questionsAnswered.sorted())
+			return questionsAnswered
 		}
-		//print(questionsAnswered.sorted())
-		return questionsAnswered
+
+		let groups = groupedLines(inputLines)
+		let setsOfLetters = groups.map { questionsAnsweredYesBy_ALL_(inGroup: $0) }
+		return setsOfLetters.map { $0.count }
 	}
 
 	override func solvePart2(inputLines: [String]) -> String {
-		let groups = groupedLines(inputLines)
-		return String(groups.reduce(0, { $0 + questionsAnsweredYesBy_ALL_(inGroup: $1).count }))
+		let total = countsForPart2(inputLines).reduce(0, { $0 + $1 })
+		return String(total)
 	}
 }
 
