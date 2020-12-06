f352ad48980a000d91fa2b53a9b2805ee068f4ed
Andy Lee <aglee@mac.com>
2020-12-06 05:48:22 -0500

    Added files in preparation for Day 7.

---
+++ AdventOfCode2020-app/Day07/Day07.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class Day07: DayNN {
+	init() {
+		super.init("DayXX")
+		self.part1Tests = [
+//			testPart1(fileNumber: 1, expectedOutput: "xxx"),
+		]
+		self.part2Tests = [
+//			testPart2(fileNumber: 1, expectedOutput: "xxx"),
+		]
+	}
+
+	// MARK: - Solving
+
+//	override func solvePart1(inputLines: [String]) -> String {
+//	}
+
+//	override func solvePart2(inputLines: [String]) -> String {
+//	}
+
+}
+
+
---
+++ AdventOfCode2020-app/Day07/Day07_Discussion.md
@@ -0,0 +1,6 @@
+# Day 7 - TBD
+
+<https://adventofcode.com/2020/day/7>
+
+
+
