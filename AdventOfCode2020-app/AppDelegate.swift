f5d68be6c97ee2a1700e72af892e89982adddb04
Andy Lee <aglee@mac.com>
2020-12-09 00:12:14 -0500

    Added files in preparation for Day 9.

--- AdventOfCode2020-app/AppDelegate.swift
+++ AdventOfCode2020-app/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 //		for d in [Day01(), Day02(), Day03(), Day04(), Day05(), Day06()] {
 //			doOneDay(d)
 //		}
-		doOneDay(Day08())
+		doOneDay(Day09())
 		NSApplication.shared.terminate(nil)
 	}
 
---
+++ AdventOfCode2020-app/Day09/Day09_Discussion.md
@@ -0,0 +1,6 @@
+# Day 9 - TBD
+
+<https://adventofcode.com/2020/day/9>
+
+
+
