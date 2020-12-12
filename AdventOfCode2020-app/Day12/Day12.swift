d8163350dc8972af2ddb0867be23e857e88c869a
Andy Lee <aglee@mac.com>
2020-12-12 01:53:27 -0500

    Solved Day 12.

--- AdventOfCode2020-app/Day12/Day12.swift
+++ AdventOfCode2020-app/Day12/Day12.swift
@@ -2,25 +2,126 @@ import Foundation
 
 class Day12: DayNN {
 	init() {
-		super.init("DayXX")
+		super.init("Rain Risk")
 		self.part1Tests = [
-//			testPart1(fileNumber: 1, expectedResult: "xxx"),
+			testPart1(fileNumber: 1, expectedResult: "25"),
 		]
 		self.part2Tests = [
-//			testPart2(fileNumber: 1, expectedResult: "xxx"),
+			testPart2(fileNumber: 1, expectedResult: "286"),
 		]
 	}
 
 	// MARK: - Solving
 
-//	override func solvePart1(inputLines: [String]) -> String {
-//		return "xxx"
-//	}
+	typealias Move = (action: String, amount: Int)
 
-//	override func solvePart2(inputLines: [String]) -> String {
-//		return "xxx"
-//	}
+	class ShipForPart1: CustomStringConvertible {
+		var x = 0
+		var y = 0
+		var dir: MoveDirection = .right
+		var manhattan: Int { return abs(x) + abs(y) }
 
+		func move(_ m: Move) {
+			switch m.action {
+			case "N": y -= m.amount
+			case "S": y += m.amount
+			case "E": x += m.amount
+			case "W": x -= m.amount
+			case "L":
+				switch m.amount {
+				case 90: dir = dir.turned(.turnLeft)
+				case 180: dir = dir.reversed()
+				case 270: dir = dir.turned(.turnRight)
+				default: abort()
+				}
+			case "R":
+				switch m.amount {
+				case 90: dir = dir.turned(.turnRight)
+				case 180: dir = dir.reversed()
+				case 270: dir = dir.turned(.turnLeft)
+				default: abort()
+				}
+			case "F":
+				let moveUnit = dir.delta
+				x += m.amount * moveUnit.x
+				y += m.amount * moveUnit.y
+			default: abort()
+			}
+		}
+
+		var description: String {
+			return "(\(x), \(y), \(dir))"
+		}
+	}
+
+	class ShipForPart2: CustomStringConvertible {
+		var x = 0
+		var y = 0
+		var wayX = 10
+		var wayY = -1
+		var manhattan: Int { return abs(x) + abs(y) }
+
+		func move(_ m: Move) {
+			switch m.action {
+			case "N": wayY -= m.amount
+			case "S": wayY += m.amount
+			case "E": wayX += m.amount
+			case "W": wayX -= m.amount
+			case "L":
+				switch m.amount {
+				case 90: (wayX, wayY) = (wayY, -wayX)
+				case 180: (wayX, wayY) = (-wayX, -wayY)
+				case 270: (wayX, wayY) = (-wayY, wayX)
+				default: abort()
+				}
+			case "R":
+				switch m.amount {
+				case 90: (wayX, wayY) = (-wayY, wayX)
+				case 180: (wayX, wayY) = (-wayX, -wayY)
+				case 270: (wayX, wayY) = (wayY, -wayX)
+				default: abort()
+				}
+			case "F":
+				x += m.amount * wayX
+				y += m.amount * wayY
+			default: abort()
+			}
+		}
+
+		var description: String {
+			return "(\(x), \(y), \(wayX), \(wayY))"
+		}
+	}
+
+	func movesFromLines(_ inputLines: [String]) -> [Move] {
+		func parseLine(_ s: String) -> Move {
+			return (String(s.prefix(1)), Int(s.suffix(from: s.index(after: s.startIndex)))! )
+		}
+
+		return inputLines.map { parseLine($0) }
+	}
+
+	override func solvePart1(inputLines: [String]) -> String {
+		let ship = ShipForPart1()
+		let moves = movesFromLines(inputLines)
+
+		for m in moves {
+			ship.move(m)
+		}
+
+		return String(ship.manhattan)
+	}
+
+	override func solvePart2(inputLines: [String]) -> String {
+		let ship = ShipForPart2()
+		let moves = movesFromLines(inputLines)
+
+		for m in moves {
+			ship.move(m)
+		}
+
+		return String(ship.manhattan)
+	}
 }
 
 
--- AdventOfCode2020-app/Day12/Day12_Discussion.md
+++ AdventOfCode2020-app/Day12/Day12_Discussion.md
@@ -1,6 +1,9 @@
-# Day 12 - TBD
+# Day 12 - Rain Risk
 
 <https://adventofcode.com/2020/day/12>
 
+Part 1 pitfall: I didn't read carefully enough, and didn't see that the "value" part of the "L" and "R" instructions was a number of degrees.  I was treating all turns as being 90 degrees.  The test passed because the test data happened to use only a turn of 90 degrees.  But I got the wrong answer for the "real" Part 1 data and had to go back and read more carefully.  I *felt* like I was being too hasty even I was submitting the answer.
+
+Part 2 pitfall: I got the math wrong on rotating the waypoint, because I was thinking of regular Cartesian coordinates, where positive y is north, but in the rest of my code positive y is south.  I didn't have this bug in Part 1 because in Part 1 I could use my `MoveDirection` enum where I'd already gotten the math right.  Fortunately I caught the bug in testing, so when I submitted my Part 2 answer it was right the first time.
 
 
