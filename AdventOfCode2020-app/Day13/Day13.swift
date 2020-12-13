aeb2fc86b84d5377e98f07ad343f2f6412f7eec4
Andy Lee <aglee@mac.com>
2020-12-13 02:17:47 -0500

    Solved Day 13.

--- AdventOfCode2020-app/Day13/Day13.swift
+++ AdventOfCode2020-app/Day13/Day13.swift
@@ -2,24 +2,77 @@ import Foundation
 
 class Day13: DayNN {
 	init() {
-		super.init("DayXX")
+		super.init("Shuttle Search")
 		self.part1Tests = [
-//			testPart1(fileNumber: 1, expectedResult: "xxx"),
+			testPart1(fileNumber: 1, expectedResult: "295"),
 		]
 		self.part2Tests = [
-//			testPart2(fileNumber: 1, expectedResult: "xxx"),
+			testPart2(fileNumber: 1, expectedResult: "1068781"),
+			test(input: "17,x,13,19", function: { return self.solvePart2($0) }, expectedResult: "3417"),
+			test(input: "67,7,59,61", function: { return self.solvePart2($0) }, expectedResult: "754018"),
+			test(input: "67,x,7,59,61", function: { return self.solvePart2($0) }, expectedResult: "779210"),
+			test(input: "67,7,x,59,61", function: { return self.solvePart2($0) }, expectedResult: "1261476"),
+			test(input: "1789,37,47,1889", function: { return self.solvePart2($0) }, expectedResult: "1202161486"),
 		]
 	}
 
 	// MARK: - Solving
 
-//	override func solvePart1(inputLines: [String]) -> String {
-//		return "xxx"
-//	}
+	override func solvePart1(inputLines: [String]) -> String {
+		func parseBusNumbers(_ s: String) -> [Int] {
+			let elements = s.components(separatedBy: ",")
+			let validElements = elements.filter { $0 != "x" }
+			return validElements.map { Int($0)! }
+		}
 
-//	override func solvePart2(inputLines: [String]) -> String {
-//		return "xxx"
-//	}
+		let earliestTime = Int(inputLines[0])!
+		let busNumbers = parseBusNumbers(inputLines[1])
+
+		var shortestWait = Int.max
+		var bestBus = -1
+		for bus in busNumbers {
+			let timeSinceLastBus = earliestTime % bus
+			if timeSinceLastBus == 0 {
+				return "0"
+			}
+			let timeToNextBus = bus - timeSinceLastBus
+			if timeToNextBus < shortestWait {
+				shortestWait = timeToNextBus
+				bestBus = bus
+			}
+		}
+
+		return String(bestBus * shortestWait)
+	}
+
+	/// The key was the Chinese Remainder Theorem.
+	func solvePart2(_ s: String) -> String {
+		let elements = s.components(separatedBy: ",")
+		var pairs: [(offset: Int, modulus: Int)] = []
+		for (i, s) in elements.enumerated() {
+			if s != "x" {
+				pairs.append((offset: i, modulus: Int(s)!))
+			}
+		}
+
+		var t = 0
+		var jumpSize = 1
+		for pair in pairs {
+			while true {
+				if (t + pair.offset) % pair.modulus == 0 {
+					break
+				}
+				t += jumpSize
+			}
+			jumpSize *= pair.modulus
+		}
+
+		return String(t)
+	}
+
+	override func solvePart2(inputLines: [String]) -> String {
+		return solvePart2(inputLines[1])
+	}
 
 }
 
