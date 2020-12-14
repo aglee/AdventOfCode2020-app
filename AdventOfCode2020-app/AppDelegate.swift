a8a740c367513714487649f31196e8cf88451761
Andy Lee <aglee@mac.com>
2020-12-14 17:21:50 -0500

    Added files in preparation for Day 15.

--- AdventOfCode2020-app/AppDelegate.swift
+++ AdventOfCode2020-app/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 //		for d in [Day01(), Day02(), Day03(), Day04(), Day05(), Day06()] {
 //			doOneDay(d)
 //		}
-		doOneDay(Day14())
+		doOneDay(Day15())
 		NSApplication.shared.terminate(nil)
 	}
 
---
+++ AdventOfCode2020-app/Day15/Day15_Discussion.md
@@ -0,0 +1,6 @@
+# Day 15 - PUT_DESCRIPTION_HERE
+
+<https://adventofcode.com/2020/day/XX>
+
+
+
