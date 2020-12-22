e247f9f88974d515f09bc9ceb5fd40454c78512a
Andy Lee <aglee@mac.com>
2020-12-22 05:09:23 -0500

    Day 22: Removed DEBUGPRINT lines.
    Even though they're commented out, their presence annoys me.

--- AdventOfCode2020-app/Day22/Day22.swift
+++ AdventOfCode2020-app/Day22/Day22.swift
@@ -62,12 +62,7 @@ class Day22: DayNN {
 
 		var hand: Hand
 
-		var gameNumber: Int
-		static var nextGameNumber = 1
-
 		init(_ deck1: [Int], _ deck2: [Int]) {
-			self.gameNumber = Game.nextGameNumber
-			Game.nextGameNumber += 1
 			self.hand = Hand(deck1: deck1, deck2: deck2)
 		}
 
@@ -78,10 +73,6 @@ class Day22: DayNN {
 
 		func playGame() -> Int {
 			let winner = _playGameRecursively()
-			//DEBUGPRINT("")
-			//DEBUGPRINT("== Post-game results ==")
-			//DEBUGPRINT("Player 1's deck: \(hand.deck1.map { String($0) }.joined(separator: ", "))")
-			//DEBUGPRINT("Player 2's deck: \(hand.deck2.map { String($0) }.joined(separator: ", "))")
 			if winner == 1 {
 				return score(hand.deck1)
 			} else if winner == 2 {
@@ -90,12 +81,10 @@ class Day22: DayNN {
 			abort()
 		}
 
+		/// Returns either 1 or 2, depending which player wins the game.
 		private func _playGameRecursively() -> Int {
-			//DEBUGPRINT("=== Game \(gameNumber) ===")
-
 			var pastHands = Set<Hand>()
-			var roundNumber = 1
-			var winnerOfGame = -1
+			var winnerOfGame: Int!
 
 			while true {
 				// Test for end-of-game.
@@ -114,28 +103,17 @@ class Day22: DayNN {
 				pastHands.insert(hand)
 
 				// Play a round.
-				//DEBUGPRINT("")
-				//DEBUGPRINT("-- Round \(roundNumber) (Game \(gameNumber)) --")
-				//DEBUGPRINT("Player 1's deck: \(hand.deck1.map { String($0) }.joined(separator: ", "))")
-				//DEBUGPRINT("Player 2's deck: \(hand.deck2.map { String($0) }.joined(separator: ", "))")
-				//DEBUGPRINT("Player 1 plays: \(hand.deck1[0])")
-				//DEBUGPRINT("Player 2 plays: \(hand.deck2[0])")
 				var winnerOfRound: Int
 				let (card1, card2) = hand.pop()
 				if card1 > hand.deck1.count || card2 > hand.deck2.count {
 					winnerOfRound = (card1 > card2 ? 1 : 2)
 				} else {
 					// If we got here, we need to recurse.
-					//DEBUGPRINT("Playing a sub-game to determine the winner...")
-					//DEBUGPRINT("")
 					let subgame = Game(Array(hand.deck1[0..<card1]),
 									   Array(hand.deck2[0..<card2]))
 					winnerOfRound = subgame._playGameRecursively()
-					//DEBUGPRINT("")
-					//DEBUGPRINT("...anyway, back to game \(gameNumber).")
 				}
 
-				//DEBUGPRINT("Player \(winnerOfRound) wins round \(roundNumber) of game \(gameNumber)!")
 				if winnerOfRound == 1 {
 					hand.deck1.append(card1)
 					hand.deck1.append(card2)
@@ -143,8 +121,6 @@ class Day22: DayNN {
 					hand.deck2.append(card2)
 					hand.deck2.append(card1)
 				}
-
-				roundNumber += 1
 			}
 
 			//DEBUGPRINT("The winner of game \(gameNumber) is player \(winnerOfGame)!")
@@ -153,7 +129,6 @@ class Day22: DayNN {
 	}
 
 	override func solvePart2(inputLines: [String]) -> String {
-		DEBUGPRINT_ENABLED = false
 		let game = Game(inputLines)
 		return String(game.playGame())
 	}
