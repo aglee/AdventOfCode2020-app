b85b7d8a283a3c8a0ffd080f2297a6cdcc690fc1
Andy Lee <aglee@mac.com>
2020-12-24 23:38:53 -0500

    Added files in preparation for Day 25.

---
+++ AdventOfCode2020-app/Day25/Day25.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class Day25: DayNN {
+	init() {
+		super.init("PUT_DESCRIPTION_HERE")
+		self.part1Tests = [
+			testPart1(fileNumber: 1, expectedResult: "xxx"),
+		]
+		self.part2Tests = [
+			testPart2(fileNumber: 1, expectedResult: "xxx"),
+		]
+	}
+
+	// MARK: - Solving
+
+	override func solvePart1(inputLines: [String]) -> String {
+		return "xxx"
+	}
+
+	override func solvePart2(inputLines: [String]) -> String {
+		return "xxx"
+	}
+}
+
+
---
+++ AdventOfCode2020-app/Day25/Day25_Discussion.md
@@ -0,0 +1,6 @@
+# Day 25 - PUT_DESCRIPTION_HERE
+
+<https://adventofcode.com/2020/day/25>
+
+
+
